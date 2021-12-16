ebad812ddf6e4e234033a08d038c0ba4abf74afd
Krisdina <77259488+krisdinakr@users.noreply.github.com>
2021-12-16 14:14:27 +0700

    WIP: dashboard page

--- src/app.tsx
+++ src/app.tsx
@@ -1,5 +1,6 @@
+import { Template } from 'components';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import { HomePage, AttractionsPage, LoginAdminPage } from './pages';
+import { HomePage, AttractionsPage, LoginAdminPage, DashboardPage } from './pages';
 import { ADMIN_ROUTES, USER_ROUTES } from './routes';
 
 const App = () => (
@@ -7,6 +8,11 @@ const App = () => (
     <Switch>
       <Route exact path={USER_ROUTES.ROOT}>
         <HomePage />
+      </Route>
+      <Route path={ADMIN_ROUTES.ROOT}>
+        <Template>
+          <DashboardPage />
+        </Template>
       </Route>
       <Route path={ADMIN_ROUTES.LOGIN}>
         <LoginAdminPage />
--- src/pages/admin/dashboard/index.tsx
+++ src/pages/admin/dashboard/index.tsx
@@ -1,6 +1,10 @@
 const DashboardPage = () => (
-  <div className="dashboard">
-    
+  <div className="container">
+    <div className="row">
+      <div className="col-md-4 col-lg-3 border rounded bg-warning">
+        Attractions
+      </div>
+    </div>
   </div>
 )
 
