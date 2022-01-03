f3a2f3a31d65d1cd031244f11fd32d2f81ca1d38
Krisdina <77259488+krisdinakr@users.noreply.github.com>
2022-01-03 20:47:54 +0700

    add static card on dashboard page

--- src/pages/admin/dashboard/index.tsx
+++ src/pages/admin/dashboard/index.tsx
@@ -1,8 +1,32 @@
+import { Link } from 'react-router-dom';
+import { ADMIN_ROUTES } from 'routes';
+
 const DashboardPage = () => (
-  <div className="container">
-    <div className="row">
-      <div className="col-md-4 col-lg-3 border rounded bg-warning">
-        Attractions
+  <div className="dashboard-page">
+    <div className="row gap-3">
+      <div className="dashboard-page__info">
+        <div className="dashboard-page__header-info">
+          <div className="dashboard-page__body-info">40</div>
+          <div className="dashboard-page__title-info">Places</div>
+        </div>
+        <Link
+          to={ADMIN_ROUTES.ATTRACTION_LIST}
+          className="dashboard-page__button-info"
+        >
+          more info
+        </Link>
+      </div>
+      <div className="dashboard-page__info">
+        <div className="dashboard-page__header-info">
+          <div className="dashboard-page__body-info">19</div>
+          <div className="dashboard-page__title-info">Categories</div>
+        </div>
+        <Link
+          to={ADMIN_ROUTES.ATTRACTION_LIST}
+          className="dashboard-page__button-info"
+        >
+          more info
+        </Link>
       </div>
     </div>
   </div>
