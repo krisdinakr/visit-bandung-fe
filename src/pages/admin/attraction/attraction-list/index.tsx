1a9d7634f5e70bf3440021ef2398b309538c3ee2
Krisdina <77259488+krisdinakr@users.noreply.github.com>
2022-01-04 16:56:46 +0700

    integrate handleDelete with backend and add alert component

--- src/pages/admin/attraction/attraction-list/index.tsx
+++ src/pages/admin/attraction/attraction-list/index.tsx
@@ -1,26 +1,60 @@
-import { useCallback, useEffect, useState } from 'react';
+import { useCallback, useEffect, useMemo, useState } from 'react';
 import { Link } from 'react-router-dom';
 import AttractionService from 'services/attractions';
 import { ADMIN_ROUTES } from 'routes';
+import { Alert, Modal } from 'components';
+import { NotificationType } from 'types';
 
 const AttractionListPage = () => {
   const [attractions, setAttractions] = useState<Array<any>>([]);
   const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [notification, setNotification] = useState<NotificationType | null>(
+    null
+  );
   const tableCol = ['name', 'category', 'subCategory'];
+  const attractionService = useMemo(() => new AttractionService(), []);
 
   const fetchApi = useCallback(async () => {
     setIsLoading(true);
-    const attractionService = new AttractionService();
     const result: any = await attractionService.get();
     if (result.status === 'success') {
       setAttractions(result.data);
     }
     setIsLoading(false);
-  }, []);
+  }, [attractionService]);
 
   useEffect(() => {
     fetchApi();
   }, [fetchApi]);
+
+  const handleDelete = useCallback(
+    async (id, name) => {
+      try {
+        const deleteResult: any = await attractionService.delete(id);
+        console.log(deleteResult);
+        if (deleteResult.status === 'success') {
+          const attractionsUpdated = attractions.filter(
+            (attraction) => attraction.id !== id
+          );
+          setAttractions(attractionsUpdated);
+          setNotification({
+            type: 'info',
+            message: `Data ${name} successfully deleted!`,
+          });
+          setTimeout(() => {
+            setNotification(null);
+          }, 4000);
+        }
+      } catch (err: any) {
+        console.log(err.message);
+        setNotification({ type: 'error', message: err.message });
+        setTimeout(() => {
+          setNotification(null);
+        }, 4000);
+      }
+    },
+    [attractionService, attractions]
+  );
 
   if (isLoading)
     return (
@@ -37,6 +71,9 @@ const AttractionListPage = () => {
 
   return (
     <div>
+      {notification && (
+        <Alert type={notification.type} message={notification.message} />
+      )}
       <table className="table table-striped table-bordered">
         <thead>
           <tr className="table-primary">
@@ -60,9 +97,24 @@ const AttractionListPage = () => {
                   >
                     <i className="fas fa-info-circle text-primary"></i>
                   </Link>
-                  <button type="button" className="btn py-0">
+                  <button
+                    type="button"
+                    className="btn py-0"
+                    data-bs-toggle="modal"
+                    data-bs-target={`#deleteModal-${row.id}`}
+                  >
                     <i className="fas fa-trash text-danger"></i>
                   </button>
+                  <Modal
+                    id={`deleteModal-${row.id}`}
+                    buttonLabel="delete"
+                    handleButton={() => handleDelete(row.id, row.name)}
+                  >
+                    <p className="text-center my-2 px-3">
+                      Do you really want to delete this records? This process
+                      cannot be undone.
+                    </p>
+                  </Modal>
                 </td>
               </tr>
             ))}
