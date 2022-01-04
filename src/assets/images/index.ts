c07beb377dc89cf046aaf2cda277ab242ac7d4bf
Krisdina <77259488+krisdinakr@users.noreply.github.com>
2022-01-04 17:27:34 +0700

    add static image for attraction interest cover

--- src/assets/images/index.ts
+++ src/assets/images/index.ts
@@ -2,5 +2,6 @@ import shopping from './interest-shopping.jpg';
 import culinary from './interest-culinary.jpg';
 import nature from './interest-nature.jpg';
 import culture from './interest-culture.jpg';
+import attractions from './interest-attractions.jpg';
 
-export { shopping, culinary, nature, culture };
+export { shopping, culinary, nature, culture, attractions };
